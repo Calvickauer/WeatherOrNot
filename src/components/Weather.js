95e08e168498f72b2fb68367ff1700cb2214656c
calvickauer <calvickauer@gmail.com>
2023-06-29 16:34:14 -0700

    add audio to welcome page

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,34 +8,59 @@ const WeatherComponent = () => {
     const fetchWeatherData = async () => {
       try {
         const apiKey = '73d42074aede0b99cf600647722fc931';
+        let url;
 
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
-        const data = await response.json();
-        data.main.temp = ((data.main.temp * 9 / 5) + 32).toFixed(2);
-        data.main.feels_like = ((data.main.feels_like * 9 / 5) + 32).toFixed(2);
-        data.main.temp_max = ((data.main.temp_max * 9 / 5) + 32).toFixed(2);
-        data.main.temp_min = ((data.main.temp_min * 9 / 5) + 32).toFixed(2);
-        data.weather[0].description = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1).toLowerCase();
-        console.log(data);
-        setWeatherData(data);
+        if (cityName) {
+          url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+        } else {
+          // Use browser geolocation to get the user's coordinates
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const { latitude, longitude } = position.coords;
+              url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+              fetchData(url);
+            },
+            (error) => {
+              console.error('Error getting geolocation:', error);
+            }
+          );
+        }
+
+        if (url) {
+          fetchData(url);
+        }
       } catch (error) {
-        console.error("My Error ", error);
+        console.error('My Error ', error);
       }
     };
 
-    if (cityName) {
-      fetchWeatherData();
-    }
+    fetchWeatherData();
   }, [cityName]);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      data.main.temp = ((data.main.temp * 9) / 5 + 32).toFixed(2);
+      data.main.feels_like = ((data.main.feels_like * 9) / 5 + 32).toFixed(2);
+      data.main.temp_max = ((data.main.temp_max * 9) / 5 + 32).toFixed(2);
+      data.main.temp_min = ((data.main.temp_min * 9) / 5 + 32).toFixed(2);
+      data.weather[0].description =
+        data.weather[0].description.charAt(0).toUpperCase() +
+        data.weather[0].description.slice(1).toLowerCase();
+      console.log(data);
+      setWeatherData(data);
+    } catch (error) {
+      console.error('My Error ', error);
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Trigger weather data fetching when the form is submitted
     setCityName(event.target.elements.city.value);
   };
-
   return (
-    <div className='weather__data'>
+    <div className="weather__data">
       <form onSubmit={handleSubmit}>
         <label htmlFor="city">Enter City:</label>
         <input type="text" id="city" name="city" />
@@ -45,16 +70,23 @@ const WeatherComponent = () => {
       {weatherData ? (
         <div>
           <h2>Weather in {weatherData.name}</h2>
-          <p className='weather__text'> Current Weather: {weatherData.weather[0].description}<img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`} alt="Weather Icon" /></p>
-          <p className='weather__text'>Temperature: {weatherData.main.temp}°F</p>
-          <p className='weather__text'>Feels like: {weatherData.main.feels_like}°F</p>
-          <p className='weather__text'>Max Temp: {weatherData.main.temp_max}°F</p>
-          <p className='weather__text'>Min Temp: {weatherData.main.temp_min}°F</p>
-          <p className='weather__text'>Humidity: {weatherData.main.humidity}%</p>
-          <p className='weather__text'>Wind Speed: {weatherData.wind.speed}</p>
+          <p className="weather__textMain">
+            {' '}
+            Current Weather: {weatherData.weather[0].description}
+            <img
+              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+              alt="Weather Icon"
+            />
+          </p>
+          <p className="weather__text0">Temperature: {weatherData.main.temp}°F</p>
+          <p className="weather__text1">Feels like: {weatherData.main.feels_like}°F</p>
+          <p className="weather__text2">Max Temp: {weatherData.main.temp_max}°F</p>
+          <p className="weather__text3">Min Temp: {weatherData.main.temp_min}°F</p>
+          <p className="weather__text4">Humidity: {weatherData.main.humidity}%</p>
+          <p className="weather__text5">Wind Speed: {weatherData.wind.speed}</p>
         </div>
       ) : (
-        <p>Enter a city name to get the weather</p>
+        <p>Loading weather data...</p>
       )}
     </div>
   );
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import audioFile from '../audio/MainSong.mp3';
 import WeatherApi from './Weather';
 
 const Welcome = () => {
-    return (
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    audioRef.current.play();
+    setIsPlaying(true);
+  }, []);
+
+  return (
+    <div>
+      <div className='weatherApi__container'>
         <div>
-            <h1>Weather or Not!</h1>
-            <p>Lets check the weather!</p>
-            <div className='weatherApi__container'>
-                < WeatherApi />
-            </div>
+          <h1>Weather or Not!</h1>
+          <p>Lets check the weather!</p>
         </div>
-    )
-}
+        <WeatherApi />
+      </div>
+
+      <div>
+        <button onClick={togglePlay}>{isPlaying ? 'Pause Music' : 'Play Music'}</button>
+      </div>
+
+      <audio ref={audioRef} src={audioFile} />
+    </div>
+  );
+};
 
 export default Welcome;
