c212685601d1737fc06729d9ba58868145ea00d6
calvickauer <calvickauer@gmail.com>
2023-08-12 23:43:50 -0700

    fix p tag postioning for Teresa page

--- src/components/Teresa.js
+++ src/components/Teresa.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Teresa = () => {
   return (
     <div className='t__body'>
-        <div className='t__main'>
-            <h1>Sexiest Woman Alive Landing Page!</h1>
-        </div>
-        <p className='t__desc'>
-
-Teresa, the sexiest woman alive, is a captivating and intelligent brunette with 
-a magnetic charm that leaves a lasting impression. 
-Her alluring presence and impeccable style make her irresistible and unforgettable if you ever get the opportunity to fuck that tasty ass pussy and sweet ass!!!
+      <div className='t__main'>
+        <h1>Sexiest Woman Alive Landing Page!</h1>
+      </div>
+      <div className='t__desc'>
+        <p>
+          Teresa, the sexiest woman alive, is a captivating and intelligent brunette with 
+          a magnetic charm that leaves a lasting impression. 
+          Her alluring presence and impeccable style make her irresistible and unforgettable if you ever get the opportunity to tear that sweet ass the fuck up!!
         </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Teresa
+export default Teresa;
