9c475ea8b2e5a89f756648f85b220760ef5708f7
calvickauer <calvickauer@gmail.com>
2023-08-16 00:06:01 -0700

    hardcoded messages displayed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,69 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios'; // Import Axios for API requests
 
 const Profile = (props) => {
-    const { handleLogout, user } = props;
-    const { id, name, email, exp } = user;
-    const expirationTime = new Date(exp * 1000);
-    let currentTime = Date.now();
+  const { handleLogout, user } = props;
+  const { id, name, email, exp } = user;
+  const expirationTime = new Date(exp * 1000);
+  let currentTime = Date.now();
 
-    const [messages, setMessages] = useState([]);
-    const [loadingMessages, setLoadingMessages] = useState(true);
+  const sampleMessages = [
+    'Hello there!',
+    'Welcome to the messages page.',
+    'You got some mufuckin mail boi!!!',
+  ];
 
-    useEffect(() => {
-        // Fetch messages here
-        axios
-            .get('/api/messages') // Update the API endpoint accordingly
-            .then((response) => {
-                setMessages(response.data.message);
-                setLoadingMessages(false);
-            })
-            .catch((error) => {
-                console.error('Error fetching messages:', error);
-                setLoadingMessages(false);
-            });
-    }, []);
+  if (currentTime >= expirationTime) {
+    handleLogout();
+    alert('Session has ended. Please login to continue.');
+  }
 
-    if (currentTime >= expirationTime) {
-        handleLogout();
-        alert('Session has ended. Please login to continue.');
-    }
+  const userData = user ? (
+    <div className="Profile__container">
+      <h1>Profile</h1>
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+      <p>ID: {id}</p>
+      <div>
+        <h2>Messages</h2>
+        <ul>
+          {sampleMessages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  ) : (
+    <h2>Loading...</h2>
+  );
 
-    const userData = user ? (
-        <div className='Profile__container'>
-            <h1>Profile</h1>
-            <p>Name: {name}</p>
-            <p>Email: {email}</p>
-            <p>ID: {id}</p>
-            <div>
-                <h2>Messages</h2>
-                {loadingMessages ? (
-                    <p>Loading messages...</p>
-                ) : (
-                    <ul>
-                        {messages.map((message, index) => (
-                            <li key={index}>{message}</li>
-                        ))}
-                    </ul>
-                )}
-            </div>
-        </div>
-    ) : (
-        <h2>Loading...</h2>
+  const errorDiv = () => {
+    return (
+      <div className="text-center pt-4">
+        <h3>
+          Please <Link to="/login">login</Link> to view this page
+        </h3>
+      </div>
     );
+  };
 
-    const errorDiv = () => {
-        return (
-            <div className="text-center pt-4">
-                <h3>
-                    Please <Link to="/login">login</Link> to view this page
-                </h3>
-            </div>
-        );
-    };
-
-    return <div className="text-center pt-4">{user ? userData : errorDiv()}</div>;
+  return <div className="text-center pt-4">{user ? userData : errorDiv()}</div>;
 };
 
 export default Profile;
