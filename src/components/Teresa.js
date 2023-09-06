a210b3da7091003eb4ac94ee09ff16eb0436a0b5
calvickauer <calvickauer@gmail.com>
2023-09-05 23:39:43 -0700

    update Teresa page

--- src/components/Teresa.js
+++ src/components/Teresa.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
+const teresaContent = {
+  title: "Sexiest Woman Alive Landing Page!",
+  description: `Teresa, the sexiest woman alive, is a captivating and intelligent brunette with 
+    a magnetic charm that leaves a lasting impression. 
+    Her alluring presence and impeccable style make her irresistible and unforgettable if you ever get the opportunity to tear that sweet ass the fuck up!!`,
+};
+
 const Teresa = () => {
   return (
     <div className='t__body'>
       <div className='t__main'>
-        <h1>Sexiest Woman Alive Landing Page!</h1>
+        <h1>{teresaContent.title}</h1>
       </div>
       <div className='t__desc'>
-        <p>
-          Teresa, the sexiest woman alive, is a captivating and intelligent brunette with 
-          a magnetic charm that leaves a lasting impression. 
-          Her alluring presence and impeccable style make her irresistible and unforgettable if you ever get the opportunity to tear that sweet ass the fuck up!!
-        </p>
+        <p>{teresaContent.description}</p>
       </div>
     </div>
   );
